8797f535b64013e8f86fd64246a7aafd6541dd57
DanielProgrammer64 <danielinternet567@gmail.com>
2024-04-29 18:50:00 -0300

    Forty Commit

--- Text.js
+++ Text.js
@@ -6,33 +6,45 @@ document.addEventListener("DOMContentLoaded", () => {
   let pesoInput = document.getElementById("peso");
   let alturaInput = document.getElementById("altura");
   let botaoCalcular = document.getElementById("calcular");
-  let Paragaph_of_result = document.getElementById("resultadoIMC");
-  const categoria = "";
+  let Paragaph_of_result = document.getElementById("resultado");
 
   // CAMPO DE DECISÃO CATEGORIA
-  botaoCalcular.addEventListener("click", () => {
+  botaoCalcular.addEventListener("click", (event) => {
+    event.preventDefault();
     console.log("Buscando categorias...");
+
+    if (!pesoInput.value) {
+      alert("informe o seu peso");
+    }
+
+    if (!alturaInput.value) {
+      alert("Informe a sua altura");
+    }
+
+    let peso = Number(pesoInput.value);
+    let altura = Number(alturaInput.value) / 100;
+    let result = peso / (altura * altura);
+
     var xhr = new XMLHttpRequest();
-    xhr.open("GET", "imc.js");
-    xhr.addEventListener("load", function () {
-      var resposta = xhr.responseText;
+    xhr.open("GET", "imc.json");
+    xhr.addEventListener("load", () => {
+      var resposta = JSON.parse(xhr.responseText);
       console.log(resposta);
+
+      let categoria = "";
+      resposta.forEach(item => {
+        if (result >= item.imc_minimo && result <= item.imc_maximo) {
+          categoria = item.categoria;
+        }
+      });
+
+      console.info(genero.value);
+      console.info(result.toFixed(1));
+
+      Paragaph_of_result.textContent = `Seu Imc:${result.toFixed(1)} 
+     \nGênero:${genero.value} \ncategoria:${categoria}`;
     });
 
     xhr.send();
-  });
-
-  // campo de função do botão
-  // cálculo de peso por altura ao quadrado
-
-  botaoCalcular.addEventListener("click", (event) => {
-    event.preventDefault();
-    console.info(genero.value);
-    let peso = Number(pesoInput.value);
-    let altura = Number(alturaInput.value) / 100;
-    let resultado = peso / (altura * altura);
-    console.info(resultado.toFixed(1));
-    Paragaph_of_result.textContent = `Seu Imc:${resultado.toFixed(1)} \r 
-   e seu Gênero:${genero.value} categoria:${categoria}`;
   });
 });
